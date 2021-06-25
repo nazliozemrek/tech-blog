821efd556d25bec895840bf8963074ea01f83035
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-06-25 10:36:28 -0500

    Post

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,94 @@
+const router = require('express').Router();
+const {Post,User} = require('../../models');
+
+router.get('/',(req,res)=> {
+    Post.findAll({
+        attributes:['id','title','created_at'],
+        order:[['created_at','DESC']],
+        include:[
+            {
+                model:User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+router.get('/:id',(req,res)=> {
+    Post.findOne({
+        where:{
+            id:req.params.id
+        },
+        attributes:['id','title','created_at'],
+        include:[
+            {
+                model:User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(postData => {
+        if(!postData){
+            res.status(404).json({message:'No post'});
+            return;
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+router.post('/',(req,res)=> {
+    Post.create({
+        title:req.body.title,
+        user_id:req.body.user_id
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+router.put('/:id',(req,res)=> {
+    Post.update(
+        {
+            title:req.body.title
+        },
+        {
+            where:{
+                id:req.params.id
+            }
+        }
+    )
+    .then(postData=>{
+        if(!postData){
+            res.status(404).json({message:'no post'});
+            return;
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+router.delete('/:id',(req,res) => {
+    Post.destroy({
+        where:{
+            id:req.params.id
+        }
+    })
+    .then(postData => {
+        if(!postData){
+            res.status(404).json({message:'No post'});
+            return;
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
